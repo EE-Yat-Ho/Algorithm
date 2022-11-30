2e009fa2a2a6ee38397a3eae7f72d65cf4b9337b
YoungHoPark <36946196+EE-Yat-Ho@users.noreply.github.com>
2022-11-30 20:41:05 +0900

    [Bronze II] Title: 분산처리, Time: 12 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.swift
+++ 백준/Bronze/1009. 분산처리/분산처리.swift
@@ -2,13 +2,15 @@ let N = Int(readLine()!)!
 
 for _ in 0..<N {
     let input = readLine()!.split(separator: " ")
-    let A = Int(input[0])!
-    let B = Int(input[1])!
+    let A = Int(input[0])! % 10
+    var B = Int(input[1])! % 4
     
-    var answer = A % 10
-    for _ in 1..<B {
+    if B == 0 { B = 4 }
+    
+    var answer = 1
+    for _ in 0..<B {
         answer = (answer * A) % 10
     }
-     
+
     print(answer == 0 ? "10" : answer)
 }
