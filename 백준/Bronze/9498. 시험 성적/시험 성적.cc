6c43cc301e802d600cfbd8e4161894def02dca70
YoungHoPark <36946196+EE-Yat-Ho@users.noreply.github.com>
2022-12-09 13:32:37 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+	int N;
+	scanf("%d", &N);
+	if (N > 89)
+		printf("A");
+	else if (N > 79)
+		printf("B");
+	else if (N > 69)
+		printf("C");
+	else if (N > 59)
+		printf("D");
+	else
+		printf("F");
+}
