9380a5c9f14aa81f04dcb00563bff89e379605a4
YoungHoPark <36946196+EE-Yat-Ho@users.noreply.github.com>
2023-02-17 17:02:43 +0900

    [Silver III] Title: N과 M (5), Time: 120 ms, Memory: 69112 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.swift
@@ -0,0 +1,26 @@
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let N = input[0]
+let M = input[1]
+
+let nums = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
+var arr: [Int] = []
+
+f()
+func f() {
+    if arr.count == M {
+        print(arr.map { String($0) }.joined(separator: " "))
+        arr.removeLast()
+        return
+    }
+    
+    nums.forEach {
+        if !arr.contains($0) {
+            arr.append($0)
+            f()
+        }
+    }
+    
+    if !arr.isEmpty {
+        arr.removeLast()
+    }
+}
