03774c51d0fcd136e8381752aaecd976bf431047
YoungHoPark <36946196+EE-Yat-Ho@users.noreply.github.com>
2023-02-27 10:56:39 +0900

    [Bronze IV] Title: Which Alien?, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.swift
@@ -0,0 +1,12 @@
+let antenna = Int(readLine()!)!
+let eyes = Int(readLine()!)!
+
+if antenna >= 3, eyes <= 4 {
+    print("TroyMartian")
+}
+if antenna <= 6, eyes >= 2 {
+    print("VladSaturnian")
+}
+if antenna <= 2, eyes <= 3 {
+    print("GraemeMercurian")
+}
